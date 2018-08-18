88900fcd0615742af14d983f7d70b1fd51deed1d
caseyanderson <cta@caseyanderson.com>
2018-08-18 15:45:05 -0700

    yas

--- fixed_media/loop_all/loop_all.sh
+++ fixed_media/loop_all/loop_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 THE_SCRIPT="/home/pi/SC3-RPiZeroW/fixed_media/loop_all/loop_all.scd"
-THE_SAMPLES="/home/pi/uSAMPLES"
+THE_DIR="/home/pi/uSAMPLES"
 
-./sclang -a -l sclang.yaml $THE_SCRIPT $THE_SAMPLES
+./sclang -a -l sclang.yaml $THE_SCRIPT $THE_DIR
