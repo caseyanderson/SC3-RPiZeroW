a70f5cb1ab79d762b12272d43bbf5c5bee070c25
caseyanderson <cta@caseyanderson.com>
2018-08-18 15:32:47 -0700

    fixes

--- fixed_media/loop_one/loop_one.sh
+++ fixed_media/loop_one/loop_one.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-THE_SCRIPT="/home/pi/SC3-RPiZeroW/fixed_media/loop_all/loop_all.scd"
-THE_SAMPLES="/home/pi/uSAMPLES/"
+THE_SCRIPT="/home/pi/SC3-RPiZeroW/fixed_media/loop_one/loop_one.scd"
+THE_SAMPLE="/home/pi/uSAMPLES/akonting.aif"
 
-./sclang -a -l sclang.yaml $THE_SCRIPT $THE_SAMPLES
+./sclang -a -l sclang.yaml $THE_SCRIPT $THE_SAMPLE
