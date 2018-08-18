4c9e414acd931d9da2c7ef98d8354f4481f638fa
caseyanderson <cta@caseyanderson.com>
2018-08-18 15:41:57 -0700

    cleaner i think

--- fixed_media/loop_one/loop_one.sh
+++ fixed_media/loop_one/loop_one.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 THE_SCRIPT="/home/pi/SC3-RPiZeroW/fixed_media/loop_one/loop_one.scd"
-THE_SAMPLE="/home/pi/uSAMPLES/akonting.aif"
+THE_DIR="/home/pi/uSAMPLES"
+THE_FILE="akonting.aif"
 
-./sclang -a -l sclang.yaml $THE_SCRIPT $THE_SAMPLE
+THE_PATH=$THE_DIR/$THE_FILE
+
+./sclang -a -l sclang.yaml $THE_SCRIPT $THE_PATH
